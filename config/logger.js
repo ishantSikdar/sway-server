98238c95720bfd7f827d9933b7d47a56c2d47e0e
ishantSikdar <ishant9715@gmail.com>
2024-04-09 14:16:10 +0530

    DailyRotate Logging Applied

--- config/logger.js
+++ config/logger.js
@@ -1,13 +1,25 @@
 const winston = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
 
+// Define log format
+const logFormat = winston.format.printf(info => `[${info.timestamp}] [${info.level.toUpperCase()}] ${info.message}`);
+
+// Create logger
 exports.logger = winston.createLogger({
     level: 'debug',
     format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.printf(info => `[${info.timestamp}] [${info.level.toUpperCase()}] [${info.message}]`)
+        logFormat
     ),
     transports: [
+        // Console transport
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'logs/combined.log' })
+        // Daily Rotate File transport
+        new DailyRotateFile({
+            filename: 'logs/sway-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            maxSize: '20m', // Set the size limit for each log file, here 20 megabytes
+            maxFiles: '14d' // Retain 14 days worth of logs
+        })
     ]
 });
