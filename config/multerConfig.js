b5e91c61adec1304795474f1fd1ab1d881241631
ishantSikdar <ishant9715@gmail.com>
2024-06-17 18:52:18 +0530

    Remove local saving of images

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -1,18 +1,4 @@
 const multer = require('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // Specify the directory where files will be stored
-    },
-    filename: function (req, file, cb) {
-        // Generate a unique filename
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname)); // Keep the original file extension
-    }
-});
-
-// Initialize multer instance with defined storage
-const multerUpload = multer({ storage: storage });
+const multerUpload = multer();
 
 module.exports = { multerUpload }
