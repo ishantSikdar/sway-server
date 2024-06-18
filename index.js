a014362bdf48cc9f35d83ad516d6daa936501760
ishantSikdar <ishant9715@gmail.com>
2024-06-19 00:09:34 +0530

    Removed wss

--- index.js
+++ index.js
@@ -13,57 +13,57 @@ const expressServer = app.listen(PORT, () => {
     logger.info(`App listening to PORT ${PORT}`);
 });
 
-const wss = new WebSocket.Server({ server: expressServer });
-const clients = new Map();
+// const wss = new WebSocket.Server({ server: expressServer });
+// const clients = new Map();
 
-wss.on('connection', (socket) => {
+// wss.on('connection', (socket) => {
 
-    console.log("Client connected");
+//     console.log("Client connected");
 
-    socket.on('error', () => {
-        console.log("error occured");
-    })
+//     socket.on('error', () => {
+//         console.log("error occured");
+//     })
 
-    socket.on('message', async (data, isBinary) => {
+//     socket.on('message', async (data, isBinary) => {
 
-        // {
-        //     "userId": "66642fc2fdd4078a7e92065f",
-        //     "communityId": "6670888ec647c2474610ac9d",
-        //     "content": "newwwwwwwwwwwwwwww"
-        // }
+//         // {
+//         //     "userId": "66642fc2fdd4078a7e92065f",
+//         //     "communityId": "6670888ec647c2474610ac9d",
+//         //     "content": "newwwwwwwwwwwwwwww"
+//         // }
 
-        const messageData = JSON.parse(data);
-        const userId = messageData.userId;
+//         const messageData = JSON.parse(data);
+//         const userId = messageData.userId;
 
-        clients.set(socket, { userId });
-        console.log(clients.values());
+//         clients.set(socket, { userId });
+//         console.log(clients.values());
 
-        const community = await Community.findById(messageData.communityId);
-        console.log(community);
+//         const community = await Community.findById(messageData.communityId);
+//         console.log(community);
  
-        if (community && community.members.includes(userId)) {
-            const messageSaved = new Message({
-                userId: userId,
-                content: { data: messageData.content },
-            });
-            console.log(messageSaved);
-        //     // await messageSaved.save();
+//         if (community && community.members.includes(userId)) {
+//             const messageSaved = new Message({
+//                 userId: userId,
+//                 content: { data: messageData.content },
+//             });
+//             console.log(messageSaved);
+//         //     // await messageSaved.save();
 
-            community.members.forEach((memberId) => {
-                const memberSocket = Array.from(clients.keys()).find((clientSockets) => clients.get(clientSockets).userId === memberId.toString());
+//             community.members.forEach((memberId) => {
+//                 const memberSocket = Array.from(clients.keys()).find((clientSockets) => clients.get(clientSockets).userId === memberId.toString());
 
-                if (memberSocket && memberSocket.readyState === WebSocket.OPEN) {
-                    memberSocket.send(JSON.stringify({ senderId: userId, message: messageData.content, communityId: messageData.communityId }), { binary: isBinary });
-                }
-            });
-        }
-    });
+//                 if (memberSocket && memberSocket.readyState === WebSocket.OPEN) {
+//                     memberSocket.send(JSON.stringify({ senderId: userId, message: messageData.content, communityId: messageData.communityId }), { binary: isBinary });
+//                 }
+//             });
+//         }
+//     });
 
-    socket.on('close', () => {
-        // Remove socket from clients map on close
-        clients.delete(socket);
-        console.log('Client disconnected');
-    });
-})
+//     socket.on('close', () => {
+//         // Remove socket from clients map on close
+//         clients.delete(socket);
+//         console.log('Client disconnected');
+//     });
+// })
 
-module.exports = expressServer;
+// module.exports = expressServer;
