31cec833d30f4b355714011323766fc62101ee71
ishantSikdar <ishant9715@gmail.com>
2024-06-25 20:54:13 +0530

    Restructured folder, disabled websocket server

--- src/index.js
+++ src/index.js
@@ -13,37 +13,37 @@ const expressServer = app.listen(PORT, () => {
 });
 
 // Websocket server
-const wss = new WebSocket.Server({ server: expressServer });
-const clients = new Map();
+// const wss = new WebSocket.Server({ server: expressServer });
+// const clients = new Map();
 
-wss.on('connection', async (socket, req) => {
+// wss.on('connection', async (socket, req) => {
 
-    const connectionStatus = await establistChatConnection(socket, req);
-    const user = connectionStatus.user;
+//     const connectionStatus = await establistChatConnection(socket, req);
+//     const user = connectionStatus.user;
 
-    if (!connectionStatus.status) {
-        logger.error("Connection Failed, reason " + connectionStatus.message);
-        socket.close(1008, connectionStatus.message);
-        return;
-    }
+//     if (!connectionStatus.status) {
+//         logger.error("Connection Failed, reason " + connectionStatus.message);
+//         socket.close(1008, connectionStatus.message);
+//         return;
+//     }
 
-    clients.set(socket, { userId: socket.userId, communityId: socket.communityId });
-    logger.info(`Total Clients: ${clients.size}`);
+//     clients.set(socket, { userId: socket.userId, communityId: socket.communityId });
+//     logger.info(`Total Clients: ${clients.size}`);
 
-    socket.on('message', async (data) => {
-        const messageData = JSON.parse(data);
-        sendMessageToClients(messageData, user, clients, socket);
-    });
+//     socket.on('message', async (data) => {
+//         const messageData = JSON.parse(data);
+//         sendMessageToClients(messageData, user, clients, socket);
+//     });
 
-    socket.on('close', () => {
-        clients.delete(socket);
-        logger.info(`Client ${socket.userId} disconnected`);
-        logger.info(`Total Clients: ${clients.size}`);
-    });
+//     socket.on('close', () => {
+//         clients.delete(socket);
+//         logger.info(`Client ${socket.userId} disconnected`);
+//         logger.info(`Total Clients: ${clients.size}`);
+//     });
 
-    socket.on('error', (err) => {
-        logger.info("error occured" + err);
-    })
-})
+//     socket.on('error', (err) => {
+//         logger.info("error occured" + err);
+//     })
+// })
 
 module.exports = expressServer;
