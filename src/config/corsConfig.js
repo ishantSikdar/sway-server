d5a3935704c48f445ad170ad822e9250d2b3c679
ishantSikdar <ishant9715@gmail.com>
2024-06-29 00:51:52 +0530

    Updated CorsConfig.js

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -2,7 +2,7 @@ const allowedUrls = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
-    "https://sway-server.vercel.app",
+    "https://sway-client.vercel.app",
 ];
 
 module.exports = {
