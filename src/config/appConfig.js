fffdf3d87112f486d64fd8ccb044d7d9ba886ffc
ishantSikdar <ishant9715@gmail.com>
2024-06-29 00:14:56 +0530

    Add Health point API

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,27 +1,78 @@
+const cors = require('cors');
+const os = require('os');
 const express = require('express');
-const cors = require('./corsConfig');
 const userRoute = require("../route/userRoutes");
 const playlistRoute = require("../route/playlistRoutes");
 const communityRoute = require("../route/communityRoutes");
 const { ROOT_URI_USER, ROOT_URI_PLAYLIST, ROOT_URI_COMMUNITY } = require('../constant/endpoints');
 const { logger } = require('./logger');
+const packageJson = require('../../package.json');
+const { allowedUrls } = require('./corsConfig');
 
 exports.initializeApp = () => {
     const app = express();
 
-    app.use(cors);
+    app.use(cors({
+        origin: allowedUrls
+    }));
     app.use(express.json());
 
     app.use(ROOT_URI_USER, userRoute);
     app.use(ROOT_URI_PLAYLIST, playlistRoute);
     app.use(ROOT_URI_COMMUNITY, communityRoute);
 
-    app.use((error, data, req, res) => {
-        logger.error(`Error occured: ${error}`);
+    app.get('/', async (req, res) => {
+        try {
+            const memoryUsage = process.memoryUsage();
+
+            const rssMemory = `${(memoryUsage.rss / (1024 * 1024)).toFixed(2)} MB`;
+            const heapTotalMemory = `${(memoryUsage.heapTotal / (1024 * 1024)).toFixed(2)} MB`;
+            const heapUsedMemory = `${(memoryUsage.heapUsed / (1024 * 1024)).toFixed(2)} MB`;
+            const externalMemory = `${(memoryUsage.external / (1024 * 1024)).toFixed(2)} MB`;
+
+            const serverStatus = {
+                status: 'running',
+                uptime: `${process.uptime()} seconds`,
+                memoryUsage: {
+                    rss: `${rssMemory}`,
+                    heapTotal: `${heapTotalMemory}`,
+                    heapUsed: `${heapUsedMemory}`,
+                    external: `${externalMemory}`
+                },
+                osInfo: {
+                    platform: os.platform(),
+                    release: os.release(),
+                    cpuArchitecture: os.arch(),
+                    totalMemory: `${(os.totalmem() / (1024 * 1024)).toFixed(2)} MB`,
+                    freeMemory: `${(os.freemem() / (1024 * 1024)).toFixed(2)} MB`,
+                    uptime: `${os.uptime()} seconds`
+                },
+                nodeVersion: `${process.version}`,
+                expressVersion: `${packageJson.dependencies.express}`,
+                webSocketVersion: `${packageJson.dependencies.ws}`,
+                cors: allowedUrls,
+                serverConfiguration: {
+                    port: process.env.PORT || 'Default Port'
+                },
+                additionalDependencies: packageJson.dependencies,
+                logs: []
+            };
+
+            res.status(200).json(serverStatus);
+        } catch (error) {
+            logger.error(`Error occurred while fetching server information: ${error}`);
+            res.status(500).json({
+                message: `Server error: ${error.message}`
+            });
+        }
+    });
+
+    app.use((error, req, res, next) => {
+        logger.error(`Error occurred: ${error}`);
         res.status(500).json({
-            message: `Some error occured: ${error.message}`
-        })
-    })
+            message: `Server error: ${error.message}`
+        });
+    });
 
     return app;
-}
+};
--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -1,11 +1,10 @@
-const app = require("..");
-const cors = require('cors');
+const allowedUrls = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "https://sway-react.vercel.app",
+];
 
-module.exports = cors({
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:3000",
-        "https://sway-react.vercel.app",
-    ],
-})
+module.exports = {
+    allowedUrls: allowedUrls
+};
