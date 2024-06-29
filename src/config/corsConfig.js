5a8fda00baa1f66f88b483a9683b90c535a6371b
ishantSikdar <ishant9715@gmail.com>
2024-06-30 02:02:21 +0530

    Add cors url -cloudfront

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -3,6 +3,7 @@ const allowedUrls = [
     "http://localhost:5174",
     "http://localhost:3000",
     "https://sway-client.vercel.app",
+    "https://drrvigy5lr2aw.cloudfront.net",
 ];
 
 module.exports = {
