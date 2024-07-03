21d42c4836744513c80f32182c7eebd1f7061e91
ishantSikdar <ishant9715@gmail.com>
2024-07-04 02:27:47 +0530

    Parallel document save fix

--- src/service/chatSocketServices.js
+++ src/service/chatSocketServices.js
@@ -60,12 +60,12 @@ exports.sendMessageToClients = async (messageData, user, community, clients, sen
             createdAt: messageTime
         });
 
+        messageToBeSaved.save();
         clients.forEach(async (metadata, clientSocket) => {
             if (metadata.communityId === senderSocket.communityId &&
                 (senderSocket.isPrivate ? community.members.includes(ObjectId.createFromHexString(senderSocket.userId)) : true)
                 && clientSocket.readyState === WebSocket.OPEN
             ) {
-                await messageToBeSaved.save();
                 clientSocket.send(
                     JSON.stringify({
                         msgGroupId: new ObjectId(),
